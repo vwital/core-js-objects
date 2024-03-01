6c6ff8e2c73e8e7ac62df56ad812ab7f946182a3
vwital <wital128@tut.by>
2024-03-01 21:13:38 +0300

    feat: solve Rectangle function, getJSON

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -182,10 +182,13 @@ function sellTickets(queue) {
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(/* width, height */) {
-  throw new Error('Not implemented');
+function Rectangle(width, height) {
+  this.width = width;
+  this.height = height;
+  this.getArea = () => {
+    return width * height;
+  };
 }
-
 /**
  * Returns the JSON representation of specified object
  *
@@ -196,8 +199,8 @@ function Rectangle(/* width, height */) {
  *    [1,2,3]   =>  '[1,2,3]'
  *    { width: 10, height : 20 } => '{"height":10,"width":20}'
  */
-function getJSON(/* obj */) {
-  throw new Error('Not implemented');
+function getJSON(obj) {
+  return JSON.stringify(obj);
 }
 
 /**
