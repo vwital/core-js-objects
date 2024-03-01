3fcd0dbf2488b30abbb71dc6ca7776ac37d30f2d
vwital <wital128@tut.by>
2024-03-01 21:06:42 +0300

    feat: solve sellTickets

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -155,8 +155,18 @@ function makeWord(lettersObject) {
  *    sellTickets([25, 25, 50]) => true
  *    sellTickets([25, 100]) => false (The seller does not have enough money to give change.)
  */
-function sellTickets(/* queue */) {
-  throw new Error('Not implemented');
+function sellTickets(queue) {
+  let exchange = 0;
+  if (queue.length === 0) return true;
+  for (let i = 0; i < queue.length; i += 1) {
+    if (queue[i] === 25) {
+      exchange += 25;
+    } else {
+      exchange -= queue[i];
+    }
+    if (exchange < 0) return false;
+  }
+  return true;
 }
 
 /**
